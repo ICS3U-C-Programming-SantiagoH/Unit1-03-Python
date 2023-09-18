c1d9060339f5285260896b4a85dc6c5971790253
santiagohewett <santiago.hewett@stu.ocsb.ca>
2023-09-18 16:37:40 +0000

    Update Unit1-03-Python

---
+++ hello_world_style.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+# Created By Santiago Hewett
+# Date: Sep 18, 2023
+# This program displays Hello, World! With proper style.
+
+
+def main():
+    print("Hello, World! With proper style.")
+    print("I am testing my function here.")
+
+
+if __name__ == "__main__":
+    main()
